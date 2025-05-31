c3c2a02c023c44d55df6b22843cf6004a7e1491f
Hernán Veiras <hernan.veiras@gmail.com>
2025-05-31 08:05:48 -0300

    Checkpoint 1: converting this into an historical data fetcher.

--- init.sql
+++ init.sql
@@ -1,4 +1,5 @@
-CREATE TABLE IF NOT EXISTS futures_data_1m (
+-- 1m data
+CREATE TABLE IF NOT EXISTS futures_data_historical_1m (
     exchange TEXT NOT NULL,
     symbol TEXT NOT NULL,
     timestamp TIMESTAMPTZ NOT NULL,
@@ -10,4 +11,79 @@ CREATE TABLE IF NOT EXISTS futures_data_1m (
     PRIMARY KEY (exchange, symbol, timestamp)
 );
 
-SELECT create_hypertable('futures_data_1m', 'timestamp', if_not_exists => TRUE);
+SELECT create_hypertable('futures_data_historical_1m', 'timestamp', if_not_exists => TRUE);
+
+-- 5m data
+CREATE TABLE IF NOT EXISTS futures_data_historical_5m (
+    exchange TEXT NOT NULL,
+    symbol TEXT NOT NULL,
+    timestamp TIMESTAMPTZ NOT NULL,
+    open DOUBLE PRECISION NOT NULL,
+    high DOUBLE PRECISION NOT NULL,
+    low DOUBLE PRECISION NOT NULL,
+    close DOUBLE PRECISION NOT NULL,
+    volume DOUBLE PRECISION NOT NULL,
+    PRIMARY KEY (exchange, symbol, timestamp)
+);
+
+SELECT create_hypertable('futures_data_historical_5m', 'timestamp', if_not_exists => TRUE);
+
+-- 15m data
+CREATE TABLE IF NOT EXISTS futures_data_historical_15m (
+    exchange TEXT NOT NULL,
+    symbol TEXT NOT NULL,
+    timestamp TIMESTAMPTZ NOT NULL,
+    open DOUBLE PRECISION NOT NULL,
+    high DOUBLE PRECISION NOT NULL,
+    low DOUBLE PRECISION NOT NULL,
+    close DOUBLE PRECISION NOT NULL,
+    volume DOUBLE PRECISION NOT NULL,
+    PRIMARY KEY (exchange, symbol, timestamp)
+);
+
+SELECT create_hypertable('futures_data_historical_15m', 'timestamp', if_not_exists => TRUE);
+
+-- 1h data
+CREATE TABLE IF NOT EXISTS futures_data_historical_1h (
+    exchange TEXT NOT NULL,
+    symbol TEXT NOT NULL,
+    timestamp TIMESTAMPTZ NOT NULL,
+    open DOUBLE PRECISION NOT NULL,
+    high DOUBLE PRECISION NOT NULL,
+    low DOUBLE PRECISION NOT NULL,
+    close DOUBLE PRECISION NOT NULL,
+    volume DOUBLE PRECISION NOT NULL,
+    PRIMARY KEY (exchange, symbol, timestamp)
+);
+
+SELECT create_hypertable('futures_data_historical_1h', 'timestamp', if_not_exists => TRUE);
+
+-- 4h data
+CREATE TABLE IF NOT EXISTS futures_data_historical_4h (
+    exchange TEXT NOT NULL,
+    symbol TEXT NOT NULL,
+    timestamp TIMESTAMPTZ NOT NULL,
+    open DOUBLE PRECISION NOT NULL,
+    high DOUBLE PRECISION NOT NULL,
+    low DOUBLE PRECISION NOT NULL,
+    close DOUBLE PRECISION NOT NULL,
+    volume DOUBLE PRECISION NOT NULL,
+    PRIMARY KEY (exchange, symbol, timestamp)
+);
+
+SELECT create_hypertable('futures_data_historical_4h', 'timestamp', if_not_exists => TRUE);
+
+-- 1d data
+CREATE TABLE IF NOT EXISTS futures_data_historical_1d (
+    exchange TEXT NOT NULL,
+    symbol TEXT NOT NULL,
+    timestamp TIMESTAMPTZ NOT NULL,
+    open DOUBLE PRECISION NOT NULL,
+    high DOUBLE PRECISION NOT NULL,
+    low DOUBLE PRECISION NOT NULL,
+    close DOUBLE PRECISION NOT NULL,
+    volume DOUBLE PRECISION NOT NULL,
+    PRIMARY KEY (exchange, symbol, timestamp)
+);
+
+SELECT create_hypertable('futures_data_historical_1d', 'timestamp', if_not_exists => TRUE);
